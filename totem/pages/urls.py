f8517e55b0d7321b6c90ea61c28fb4adcab91b7e
Bo Lopker <lopkerk@gmail.com>
2023-05-01 11:19:50 -0700

    How it works

--- totem/pages/urls.py
+++ totem/pages/urls.py
@@ -7,7 +7,7 @@ app_name = "pages"
 urlpatterns = [
     path("team/", views.TeamView.as_view(), name="team"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
-    path("how-it-works/", TemplateView.as_view(template_name="pages/how_it_works.html"), name="how-it-works"),
+    path("how-it-works/", views.HowItWorksView.as_view(), name="how-it-works"),
     path("tos/", TemplateView.as_view(template_name="pages/tos.html"), name="tos"),
     path("privacy/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy"),
 ]
