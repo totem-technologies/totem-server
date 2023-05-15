fc302a5aece3b692d364752d2b8461c60ed55011
Bo Lopker <lopkerk@gmail.com>
2023-05-15 13:33:46 -0700

    Try again

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,8 +9,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["totem.org", "totem.kbl.io"])
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-ALLOWED_HOSTS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+ALLOWED_HOSTS.append(socket.getaddrinfo(socket.gethostname(), "http")[0][4][0])
 
 # DATABASES
 # ------------------------------------------------------------------------------
