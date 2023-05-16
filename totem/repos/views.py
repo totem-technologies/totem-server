1bb537c7811c07a57ef2c27d75922bee5d2c782d
Bo Lopker <lopkerk@gmail.com>
2023-05-15 20:08:13 -0700

    Search prompts

--- totem/repos/views.py
+++ totem/repos/views.py
@@ -10,3 +10,17 @@ class PromptListView(ListView):
 
     def get_queryset(self):
         return Prompt.objects.prefetch_related("tags")
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        json = []
+        for prompt in context["object_list"]:
+            json.append(
+                {
+                    "prompt": prompt.prompt,
+                    "tags": [tag.name for tag in prompt.tags.all()],
+                }
+            )
+        context["promptjson"] = json
+        print(context["promptjson"])
+        return context
