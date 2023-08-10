ced4cdcdf1b534594be137b565c8df7a7321f586
Bo Lopker <lopkerk@gmail.com>
2023-08-10 15:03:45 -0700

    CDN

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["totem.org", "totem.kbl.io"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["totem.org", "totem.kbl.io", "cdn.totem.org"])
 ALLOWED_HOSTS.append(socket.getaddrinfo(socket.gethostname(), "http")[0][4][0])
 
 # DATABASES
