9e9bbcd8193929f8f4326b4997dc75e4ef6658a4
Bo Lopker <lopkerk@gmail.com>
2023-09-11 15:34:55 -0700

    Sentry

--- totem/api/management/commands/totem_tasks.py
+++ totem/api/management/commands/totem_tasks.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from sentry_sdk.crons import monitor
 
 from totem.api.api import run_tasks_impl
 
@@ -7,6 +9,13 @@ class Command(BaseCommand):
     help = "Run tasks for Totem."
 
     def handle(self, *args, **options):
+        if settings.DEBUG:
+            self._doit()
+        else:
+            with monitor(monitor_slug="totem_tasks"):
+                self._doit()
+
+    def _doit(self):
         print("Running tasks...")
         run_tasks_impl()
         print("Done.")
