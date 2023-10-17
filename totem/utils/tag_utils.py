54017e9985a888d071e7860381b86ec2e66002ef
Bo Lopker <lopkerk@gmail.com>
2023-10-16 22:08:56 -0700

    Tag clean

--- totem/utils/tag_utils.py
+++ totem/utils/tag_utils.py
@@ -1,31 +1,21 @@
+import string
+
 from taggit.utils import _parse_tags
 
-_illegal_chars = [
-    "#",
-    ":",
-    "!",
-    "?",
-    "&",
-    "(",
-    ")",
-    "[",
-    "]",
-    "{",
-    "}",
-    "/",
-    "\\",
-    "<",
-    ">",
-    "*",
+_legal_chars = {
+    "-",
+    " ",
+    ",",
     "+",
-    "=",
-    "|",
-    "~",
-    "^",
-]
+}
+_legal_chars.update(string.ascii_lowercase)
+_legal_chars.update(string.digits)
 
 
 def parse_tags(tag_string: str):
-    for char in _illegal_chars:
-        tag_string = tag_string.replace(char, "")
-    return _parse_tags(tag_string)
+    tag_string = tag_string.lower()
+    clean_tag_string = ""
+    for char in tag_string:
+        if char in _legal_chars:
+            clean_tag_string += char
+    return _parse_tags(clean_tag_string)
