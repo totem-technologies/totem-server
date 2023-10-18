be2e98cfcbaac4b623724359d243db0fdd578a13
Bo Lopker <lopkerk@gmail.com>
2023-10-17 21:08:57 -0700

    No timezone

--- totem/onboard/views.py
+++ totem/onboard/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.forms import CharField, Form, IntegerField, Textarea, TextInput
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
-from timezone_field.forms import TimeZoneFormField
 
 from totem.users.models import User
 from totem.utils.slack import notify_slack
@@ -24,13 +23,11 @@ class OnboardForm(Form):
         label="Name",
         widget=TextInput(attrs={"placeholder": "First name (or nickname)"}),
     )
-    timezone = TimeZoneFormField(choices_display="WITH_GMT_OFFSET")
     age = IntegerField(required=True, initial=None, min_value=13, max_value=120, widget=TextInput())
     hopes = CharField(max_length=1000, required=False, widget=Textarea(attrs={"rows": 3}))
 
     def save(self, user: User, onboard: OnboardModel):
         user.name = self.cleaned_data.pop("name")
-        user.timezone = self.cleaned_data.pop("timezone")
         user.clean()
         user.save()
         onboard.year_born = current_year() - int(self.cleaned_data.pop("age"))
@@ -53,7 +50,7 @@ def onboard_view(request: HttpRequest):
             return redirect("users:redirect")
     else:
         age = current_year() - onboard.year_born if onboard.year_born else None
-        initial = onboard.__dict__ | {"name": user.name, "timezone": user.timezone, "age": age}
+        initial = onboard.__dict__ | {"name": user.name, "age": age}
         form = OnboardForm(initial=initial)
     return render(
         request,
