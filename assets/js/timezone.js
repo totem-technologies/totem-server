052a20e7634148feb965aa18f7197861058aa2b8
Bo Lopker <lopkerk@gmail.com>
2023-11-15 22:05:47 -0800

    Better timezones

--- assets/js/timezone.js
+++ assets/js/timezone.js
@@ -1,12 +1,14 @@
 export default function () {
   // Timezone settings. See TimezoneMiddleware in utils/middleware.py
-  // If timezone isn't set, refresh page.
+  // If timezone isn't set in cookies, set it. Pages can force a reload if they need to.
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone // e.g. "America/New_York"
-  const hasZimzone = document.cookie
+  const hasTimezone = document.cookie
     .split(";")
     .some((item) => item.trim().startsWith("totem_timezone="))
-  if (timezone && !hasZimzone) {
+  if (timezone && !hasTimezone) {
     document.cookie = `totem_timezone=${timezone}; SameSite=Strict`
-    location.reload()
+    if (window.TOTEM_DATA.ensureTimezone === true) {
+      location.reload()
+    }
   }
 }
