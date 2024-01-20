089bb23946c6c809aa11c417e0dcf0381ff81cb3
Bo Lopker <lopkerk@gmail.com>
2024-01-19 18:57:35 -0800

    Admin logs

--- totem/utils/admin.py
+++ totem/utils/admin.py
@@ -1,4 +1,50 @@
 from django.contrib import admin
+from django.contrib.admin.models import DELETION, LogEntry
+from django.urls import reverse
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 admin.site.site_header = "✨ Totem Admin ✨"
-# admin.site.index_title = "Your Company"
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = "action_time"
+
+    list_filter = ["user", "content_type", "action_flag"]
+
+    search_fields = ["object_repr", "change_message"]
+
+    list_display = [
+        "action_time",
+        "user",
+        "content_type",
+        "object_link",
+        "action_flag",
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser  # type: ignore
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = '<a href="{}">{}</a>'.format(
+                reverse(f"admin:{ct.app_label}_{ct.model}_change", args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return mark_safe(link)
+
+    object_link.admin_order_field = "object_repr"
+    object_link.short_description = "object"
