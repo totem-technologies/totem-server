21a1d253534a703afcfbd390b07e518b15c9c879
Bo Lopker <lopkerk@gmail.com>
2024-06-01 17:38:58 -0700

    Fix DDT

--- config/settings/local.py
+++ config/settings/local.py
@@ -61,17 +61,26 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa: F40
 INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
+
+
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+def show_toolbar(request):
+    return DEBUG
+
+
 DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": [
         "debug_toolbar.panels.redirects.RedirectsPanel",
         "debug_toolbar.panels.profiling.ProfilingPanel",
     ],
     "SHOW_TEMPLATE_CONTEXT": True,
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
 }
+
 
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = type("c", (), {"__contains__": lambda *a: True})()
+
 
 # django-extensions
 # ------------------------------------------------------------------------------
--- totem/utils/middleware.py
+++ totem/utils/middleware.py
@@ -1,5 +1,4 @@
 import zoneinfo
-
 from django.conf import settings
 from django.http import Http404, HttpRequest
 from django.utils import timezone
