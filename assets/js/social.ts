e58f892d39fec96b07a39fd359f06e93182a1755
Bo Lopker <lopkerk@gmail.com>
2024-12-10 00:04:11 +0500

    Shot fix?

--- assets/js/social.ts
+++ assets/js/social.ts
@@ -3,27 +3,25 @@ import { domToPng } from "modern-screenshot"
 const imgsDivs = document.querySelectorAll("[data-img]>div")
 
 for (const imgDiv of imgsDivs) {
-  console.log(imgDiv)
-  domToPng(imgDiv, {
-    scale: 2,
-    fetch: {
-      requestInit: {
-        cache: "no-cache",
+  // attach dataUrl to the button imgDiv so that the image downloads when the button is clicked
+  imgDiv.addEventListener("click", async () => {
+    const dataUrl = await domToPng(imgDiv, {
+      scale: 2,
+      fetch: {
+        requestInit: {
+          cache: "no-cache",
+        },
       },
-    },
-  }).then((dataUrl) => {
+    })
     // generate filename from current URL
     const slug = window.location.pathname
       .split("/")
       .slice(-2)[0]
       .replace("/", "-")
-    // attach dataUrl to the button imgDiv so that the image downloads when the button is clicked
-    imgDiv.addEventListener("click", () => {
-      const link = document.createElement("a")
-      link.href = dataUrl
-      link.download = `${slug}.png`
-      console.log(link)
-      link.click()
-    })
+    const link = document.createElement("a")
+    link.href = dataUrl
+    link.download = `${slug}.png`
+    console.log(link)
+    link.click()
   })
 }
