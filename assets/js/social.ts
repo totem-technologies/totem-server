b9ed5f136374644f5643077f2e09eca3646e33ed
Bo Lopker <lopkerk@gmail.com>
2024-12-10 00:18:05 +0500

    Shot fix?

--- assets/js/social.ts
+++ assets/js/social.ts
@@ -7,11 +7,6 @@ for (const imgDiv of imgsDivs) {
   imgDiv.addEventListener("click", async () => {
     const dataUrl = await domToPng(imgDiv, {
       scale: 2,
-      fetch: {
-        requestInit: {
-          cache: "no-cache",
-        },
-      },
     })
     // generate filename from current URL
     const slug = window.location.pathname
