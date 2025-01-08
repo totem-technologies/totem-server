aeab3220a2bfd8016df10d415eb95cb1c1491d43
Bo Lopker <lopkerk@gmail.com>
2025-01-07 17:15:35 -0800

    Fix TOT-232: Allow for easier finding of 'Hopes'

--- totem/onboard/admin.py
+++ totem/onboard/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
+from django.db.models import Q
+
+from totem.utils.admin import ExportCsvMixin
 
 from .models import OnboardModel
 
 
+class HasHopesFilter(admin.SimpleListFilter):
+    title = "Has Hopes"  # Display name
+    parameter_name = "has_hopes"  # URL parameter
+
+    def lookups(self, request, model_admin):
+        return (
+            ("yes", "Yes"),
+            ("no", "No"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            return queryset.exclude(hopes__isnull=True).exclude(hopes="")
+        if self.value() == "no":
+            return queryset.filter(Q(hopes__isnull=True) | Q(hopes=""))
+
+
 @admin.register(OnboardModel)
-class OnboardAdmin(admin.ModelAdmin):
+class OnboardAdmin(ExportCsvMixin, admin.ModelAdmin):
+    actions = ["export_as_csv"]
     list_display = ["user", "onboarded", "created", "updated"]
-    list_filter = ("created", "updated")
+    list_filter = (
+        "created",
+        "updated",
+        HasHopesFilter,
+    )
     readonly_fields = ("user_name",)
