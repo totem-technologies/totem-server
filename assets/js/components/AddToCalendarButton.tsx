e252942d08fdf2375453fd460d1ad0068b2bdbf6
Bo Lopker <lopkerk@gmail.com>
2025-01-22 17:13:20 -0800

    Fix add to cal end time

--- assets/js/components/AddToCalendarButton.tsx
+++ assets/js/components/AddToCalendarButton.tsx
@@ -1,28 +1,31 @@
-import { convertISOToHHMM, getDateFromISOString } from "@/libs/time"
-import { getTimeZone } from "@/libs/timezone"
+import { convertISOToHHMM, getDateFromISOString } from "@/libs/time";
+import { getTimeZone } from "@/libs/timezone";
 function AddToCalendarButton(props: {
-  name: string
-  calLink: string
-  start: string
-  durationMinutes: number
+	name: string;
+	calLink: string;
+	start: string;
+	durationMinutes: number;
 }) {
-  // let debug = "true"
-  const startDate = () => getDateFromISOString(props.start)
-  const startTime = () => convertISOToHHMM(props.start)
+	// let debug = "true"
+	const startDate = () => getDateFromISOString(props.start);
+	const startTime = () => convertISOToHHMM(props.start);
 
-  // Add duration to end date
-  const endDateObj = () => new Date(props.start)
-  endDateObj().setMinutes(endDateObj().getMinutes() + props.durationMinutes)
-  const endDate = () => getDateFromISOString(endDateObj().toISOString())
-  const endTime = () => convertISOToHHMM(endDateObj().toISOString())
+	// Add duration to end date
+	const endDateObj = () => {
+		const endDate = new Date(props.start);
+		endDate.setMinutes(endDate.getMinutes() + props.durationMinutes);
+		return endDate;
+	};
+	const endDate = () => getDateFromISOString(endDateObj().toISOString());
+	const endTime = () => convertISOToHHMM(endDateObj().toISOString());
 
-  // Sanitize strings
-  const name = () => `${props.name}`.replaceAll('"', "")
-  const calLink = () => `${props.calLink}?r=cal_link`.replaceAll('"', "")
+	// Sanitize strings
+	const name = () => `${props.name}`.replaceAll('"', "");
+	const calLink = () => `${props.calLink}?r=cal_link`.replaceAll('"', "");
 
-  // console.log(props)
-  const debug = globalThis.TOTEM_DATA.debug ? "true" : "false"
-  const el = `<add-to-calendar-button
+	// console.log(props)
+	const debug = globalThis.TOTEM_DATA.debug ? "true" : "false";
+	const el = `<add-to-calendar-button
       styleLight="--btn-shadow:none; --btn-shadow-hover:none"
       inline
       hideBranding
@@ -36,9 +39,9 @@ function AddToCalendarButton(props: {
       endDate="${endDate()}"
       startTime="${startTime()}"
       endTime="${endTime()}"
-      timeZone="${getTimeZone()}"></add-to-calendar-button>`
-  // eslint-disable-next-line solid/no-innerhtml
-  return <div innerHTML={el} />
+      timeZone="${getTimeZone()}"></add-to-calendar-button>`;
+	// eslint-disable-next-line solid/no-innerhtml
+	return <div innerHTML={el} />;
 }
 
-export default AddToCalendarButton
+export default AddToCalendarButton;
