009a4b7d3bb3c70ee0b8a2407d9e8345ef11e674
Bo Lopker <lopkerk@gmail.com>
2025-01-25 16:50:38 -0800

    Fix blog model

--- totem/blog/admin.py
+++ totem/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.utils.html import mark_safe
+from django.utils.safestring import mark_safe
+
 from .models import BlogPost
+
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
@@ -8,19 +10,26 @@ class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("title", "header_image_preview", "slug", "date_published")
     search_fields = ("title", "content")
     prepopulated_fields = {"slug": ("title",)}
-    
+
     def header_image_preview(self, obj):
         if obj.header_image:
             return mark_safe(f'<img src="{obj.header_image.url}" style="max-height: 50px; max-width: 100px;" />')
         return "-"
+
     header_image_preview.short_description = "Header Preview"
 
     fieldsets = (
-        ("Header", {
-            "fields": ("title", "slug", "header_image"),
-        }),
-        ("Content", {
-            "fields": ("content",),
-            "classes": ("wide",),
-        }),
+        (
+            "Header",
+            {
+                "fields": ("title", "slug", "header_image"),
+            },
+        ),
+        (
+            "Content",
+            {
+                "fields": ("content",),
+                "classes": ("wide",),
+            },
+        ),
     )
--- totem/blog/models.py
+++ totem/blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from totem.utils.models import SluggedModel, AdminURLMixin
-from totem.utils.md import MarkdownMixin, MarkdownField
 
-class BlogPost(SluggedModel, AdminURLMixin, MarkdownMixin):
+from totem.utils.md import MarkdownField, MarkdownMixin
+from totem.utils.models import AdminURLMixin, SluggedModel
+
+
+class BlogPost(AdminURLMixin, MarkdownMixin, SluggedModel):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(
-        upload_to="blog/headers/%Y/%m/%d/",
-        blank=True,
-        help_text="Header image for blog post (PNG, JPG, max 5MB)"
+        upload_to="blog/headers/%Y/%m/%d/", blank=True, help_text="Header image for blog post (PNG, JPG, max 5MB)"
     )
     content = MarkdownField(
         help_text="Markdown content for the blog post",
@@ -16,7 +16,7 @@ class BlogPost(SluggedModel, AdminURLMixin, MarkdownMixin):
 
     def __str__(self):
         return self.title
-        
+
     def clean(self):
         super().clean()
         self.validate_markdown(self.content)  # Add markdown validation
