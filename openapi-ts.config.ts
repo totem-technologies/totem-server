ffadb4972c9a2dc38ddba5cb24a138e4fd102da9
Bo Lopker <lopkerk@gmail.com>
2025-02-27 21:34:09 -0800

    Update heyapi

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
-  client: "@hey-api/client-fetch",
+  plugins: ["@hey-api/client-fetch"],
   input: "openapi.json",
   output: "assets/js/client",
 })
