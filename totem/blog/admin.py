ae29ebfc2b56f2e6c083b5fb2f815654278a656b
Bo Lopker <lopkerk@gmail.com>
2025-03-03 17:54:41 -0800

    Add blog header image to admin list

--- totem/blog/admin.py
+++ totem/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from totem.users.models import User
 
@@ -22,10 +23,17 @@ class AuthorDropdownFilter(admin.SimpleListFilter):
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     save_on_top = True
-    list_display = ("title", "author__email", "slug", "date_published")
+    list_display = ("title", "author__email", "get_header_image", "date_published", "publish")
     list_filter = [AuthorDropdownFilter]
     search_fields = ("title", "subtitle", "content")
     autocomplete_fields = ["author"]
+
+    def get_header_image(self, obj):
+        if obj.header_image:
+            return mark_safe(f'<img src="{obj.header_image.url}" height="50" />')
+        return "-"
+
+    get_header_image.short_description = "Header Image"  # type: ignore
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, change, **kwargs)
