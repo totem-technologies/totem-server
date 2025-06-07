1c9b754e4f274ef5501fdb25448bf8a4c1c480c3
Bruno D'Luka <brunodlukaa@gmail.com>
2025-06-07 12:50:10 -0300

    feat: Pagination, filtering and others

--- totem/blog/mobile_api.py
+++ totem/blog/mobile_api.py
@@ -3,20 +3,37 @@ from typing import List, Optional
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from ninja import Field, FilterSchema, ModelSchema, Router, Schema
+from ninja.pagination import paginate
 
 from totem.users.models import User
+from totem.users.schemas import PublicUserSchema
 from .models import BlogPost
 
 from ninja import ModelSchema
 
-class AuthorSchema(ModelSchema):
-    class Meta:
-        model = User
-        fields = ["name", "slug"]
-
 
 class BlogPostSchema(ModelSchema):
-    author: Optional[AuthorSchema] = None
+    author: Optional[PublicUserSchema] = None
+    header_image_url: Optional[str] = None
+    content_html: Optional[str] = None
+
+    @staticmethod
+    def resolve_header_image_url(obj) -> Optional[str]:
+        if obj.header_image and hasattr(obj.header_image, 'url'):
+            return obj.header_image.url
+        return None
+    
+    @staticmethod
+    def resolve_content_html(obj: BlogPost) -> str:
+        return obj.content_html
+
+    class Meta:
+        model = BlogPost
+        fields = ["title", "subtitle", "content", "date_published", "slug", "author", "publish"]
+
+
+class BlogPostListSchema(ModelSchema):
+    author: Optional[PublicUserSchema] = None
     header_image_url: Optional[str] = None
 
     @staticmethod
@@ -27,23 +44,27 @@ class BlogPostSchema(ModelSchema):
 
     class Meta:
         model = BlogPost
-        fields = ["title", "subtitle", "content", "date_published", "slug", "author", "publish"]
+        fields = ["title", "subtitle", "date_published", "slug", "author"]
+
 
 blog_router = Router()
 
-@blog_router.get("/posts", response={200: List[BlogPostSchema]}, tags=["blog"], url_name="list_posts")
+
+@blog_router.get("/posts", response={200: List[BlogPostListSchema]}, tags=["blog"], url_name="list_posts")
+@paginate
 def list_posts(request):
     """List all blog posts"""
     
     if request.user.is_staff:
-        return BlogPost.objects.all()
-    return BlogPost.objects.filter(publish=True)
+        return BlogPost.objects.all().select_related("author")
+    return BlogPost.objects.filter(publish=True).order_by("-date_published").select_related("author")
+
 
 @blog_router.get("/post/{slug}", response=BlogPostSchema, tags=["blog"], url_name="get_post")
 def post(request, slug: str):
     if request.user.is_staff:
-        post = get_object_or_404(BlogPost, slug=slug)
+        post = get_object_or_404(BlogPost.objects.select_related("author"), slug=slug)
     else:
-        post = get_object_or_404(BlogPost, slug=slug, publish=True)
+        post = get_object_or_404(BlogPost.objects.select_related("author"), slug=slug, publish=True)
     
     return post
