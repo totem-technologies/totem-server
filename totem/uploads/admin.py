4b884f9fc1458beed9b810e55bfa603353cdab5c
Bo Lopker <lopkerk@gmail.com>
2025-09-30 21:18:07 -0700

    Blog updates

--- totem/uploads/admin.py
+++ totem/uploads/admin.py
@@ -1,3 +1,4 @@
+from typing import final
 from django import forms
 from django.contrib import admin
 from django.core.exceptions import ValidationError
@@ -21,21 +22,22 @@ class ImageAdminForm(forms.ModelForm):
         return image
 
 
+@final
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     form = ImageAdminForm
     list_display = ("title", "thumbnail_preview", "slug", "date_created")
     search_fields = ("title", "slug")
     list_filter = ("date_created",)
-    fields = ["title", "image", "slug", "image_tag", "date_created", "date_modified"]
-    readonly_fields = ("date_created", "date_modified", "image_tag", "slug")
+    fields = ["title", "image", "slug", "markdown_code", "image_tag", "date_created", "date_modified"]
+    readonly_fields = ("date_created", "date_modified", "image_tag", "slug", "markdown_code")
     ordering = ("-date_created",)
     date_hierarchy = "date_created"
     list_per_page = 20
     list_max_show_all = 200
     save_on_top = True
 
-    def image_tag(self, obj):
+    def image_tag(self, obj: Image):
         if obj and obj.image:
             return mark_safe(
                 f'<a href="{escape(obj.image.url)}" target="_blank">'
@@ -45,7 +47,10 @@ class ImageAdmin(admin.ModelAdmin):
             )
         return "No image uploaded yet."
 
-    def thumbnail_preview(self, obj):
+    def markdown_code(self, obj: Image):
+        return f'{{% image slug="{obj.slug}" %}}'
+
+    def thumbnail_preview(self, obj: Image):
         if obj and obj.image:
             # Link to the change page rather than the raw image
             change_url = reverse("admin:uploads_image_change", args=(obj.pk,))
@@ -57,5 +62,5 @@ class ImageAdmin(admin.ModelAdmin):
             )
         return "No image"
 
-    thumbnail_preview.short_description = "Preview"  # type: ignore
-    image_tag.short_description = "Image Preview"  # type: ignore
+    thumbnail_preview.short_description = "Preview"  # pyright: ignore[reportFunctionMemberAccess]
+    image_tag.short_description = "Image Preview"  # pyright: ignore[reportFunctionMemberAccess]
