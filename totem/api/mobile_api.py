7da0525949f55999fb77fdbcab568b8c443527cd
Bruno D'Luka <brunodlukaa@gmail.com>
2025-10-18 11:39:04 -0300

    Allow async calls in mobile API

--- totem/api/mobile_api.py
+++ totem/api/mobile_api.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import jwt
+from asgiref.sync import sync_to_async
 from django.conf import settings
 from django.http import HttpRequest
 from django.utils import timezone
@@ -22,7 +23,7 @@ from .auth import JWTSchema
 
 
 class JWTAuth(HttpBearer):
-    def authenticate(self, request: HttpRequest, token: str) -> Optional[User]:
+    async def authenticate(self, request: HttpRequest, token: str) -> Optional[User]:
         try:
             # Decode JWT token
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
@@ -34,7 +35,7 @@ class JWTAuth(HttpBearer):
 
             # Get user
             try:
-                user = User.objects.get(api_key=data.api_key, pk=data.pk)
+                user = await sync_to_async(User.objects.get)(api_key=data.api_key, pk=data.pk)
 
                 # Check if user is active
                 if not user.is_active:
