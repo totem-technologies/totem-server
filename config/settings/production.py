5c84a953f0a85e666b886eafed4140a29a65ee93
Bo Lopker <lopkerk@gmail.com>
2026-03-10 13:22:20 -0700

    Set strict-origin-when-cross-origin

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,6 +63,8 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS
 SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-referrer-policy
+SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
 # CSP (Content Security Policy)
 # ------------------------------------------------------------------------------
